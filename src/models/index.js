706306fe9423fc933cc5fcbd498d9d9d33853d1e
keemzyddev <hakeemdurogbola@gmail.com>
2023-02-13 13:09:23 +0100

    removed babelrc and the comments in some files

--- src/models/index.js
+++ src/models/index.js
@@ -1,49 +1,5 @@
-// 'use strict';
-
-// const fs = require('fs');
-// const path = require('path');
-// const Sequelize = require('sequelize');
-// const process = require('process');
-// const basename = path.basename(__filename);
-// const env = process.env.NODE_ENV || 'development';
-// const config = require(__dirname + '/../config/config.json')[env];
-// const db = {};
-
-// let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
-
-// fs
-//   .readdirSync(__dirname)
-//   .filter(file => {
-//     return (
-//       file.indexOf('.') !== 0 &&
-//       file !== basename &&
-//       file.slice(-3) === '.js' &&
-//       file.indexOf('.test.js') === -1
-//     );
-//   })
-//   .forEach(file => {
-//     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-//     db[model.name] = model;
-//   });
-
-// Object.keys(db).forEach(modelName => {
-//   if (db[modelName].associate) {
-//     db[modelName].associate(db);
-//   }
-// });
-
-// db.sequelize = sequelize;
-// db.Sequelize = Sequelize;
-
-// module.exports = db;
-
 import fs from "fs";
-import path, { join } from "path";
+import path from "path";
 import Sequelize from "sequelize";
 import process from "process";
 import { fileURLToPath } from "url";
@@ -52,11 +8,7 @@ const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 const basename = path.basename(filename);
 
-import { createRequire } from "node:module";
-const require = createRequire(import.meta.url);
-
-const env = process.env.NODE_ENV || "development";
-const config = require(dirname + "/../config/config.json")[env];
+import config from "../config/config.js";
 
 const db = {};
 
@@ -65,12 +17,7 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
+  sequelize = new Sequelize(config.development);
 }
 
 sequelize
@@ -85,14 +32,6 @@ sequelize
       (file) =>
         file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
     );
-
-  // await Promise.all(
-  //   files.map(async (file) => {
-  //     const module = await import(path.join(dirname, file));
-  //     const model = module.default(sequelize, Sequelize);
-  //     db[model.name] = model;
-  //   })
-  // );
 
   for await (const file of files) {
     const model = await import(`./${file}`);
